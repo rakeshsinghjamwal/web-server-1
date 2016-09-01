0f82f03aafd066e6d057aa5397fdc1f5828487d1
The coder <rakeshsinghjamwal@gmail.com>
2016-09-01 06:41:31 +0530

    Data validation in post

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 var express = require('express'); 
 var bodyParser = require('body-parser');
+var _ = require('underscore');
 
 var app = express(); 
 var PORT = process.env.PORT || 3000;
@@ -20,14 +21,7 @@ app.get('/todos',function(req, res){
 // GET /todos/:id
 app.get('/todos/:id', function (req, res) {
 	var todoItemId = parseInt(req.params.id, 10);
-	var todoItem;
-	todos.forEach(function(item){
-		if( item.id === todoItemId )
-		{
-			todoItem = item;
-		}
-	});
-
+	var todoItem  = _.findWhere(todos, {id: todoItemId});
 	if(!todoItem)
 	{
 		res.status(404).send();
@@ -40,7 +34,11 @@ app.get('/todos/:id', function (req, res) {
 
 // POST /todos
 app.post('/todos',function(req, res){
-	var body = req.body;
+	var body = _.pick(req.body, 'description','completed') ;
+	if(!_.isBoolean(body.completed) || !_.isString(body.description) || body.description.trim().length === 0){
+		return res.status(404).send();
+	}
+	body.description = body.description.trim();
 	body.id = todoNextId;
 	todos.push(body);
 	res.json(body);
